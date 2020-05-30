3b5a5091c0f8f44b6c3fb97dffc30ef88868f57d
Chatdanai Phakaket <zchatdanai@gmail.com>
2020-05-31 02:44:45 +0700

    Fix bug background and position of container

--- src/views/pages/AboutMe.vue
+++ src/views/pages/AboutMe.vue
@@ -1,43 +1,43 @@
 <template>
-<div class="text-center">
+  <div class="text-center">
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-        <header class="masthead mb-auto">
-            <div class="inner">
-                <img src="../../assets/images/logo_vetiver.png" class="masthead-brand" width="15%" />
-                <nav class="nav nav-masthead justify-content-center">
-                    <a class="nav-link" href="http://localhost:8080/homepage">หน้าหลัก</a>
-                    <a class="nav-link" href="http://localhost:8080/aboutme">เกี่ยวกับเรา</a>
-                    <a class="nav-link" href="http://localhost:8080/contact">คุยกับเรา</a>
-                    <a class="nav-link" href="#">ร่วมงานกับเรา</a>
-                </nav>
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <img src="../../assets/images/logo_vetiver.png" class="masthead-brand" />
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="http://localhost:8080/homepage">หน้าหลัก</a>
+            <a class="nav-link" href="http://localhost:8080/aboutme">เกี่ยวกับเรา</a>
+            <a class="nav-link" href="http://localhost:8080/contact">คุยกับเรา</a>
+            <a class="nav-link" href="#">ร่วมงานกับเรา</a>
+          </nav>
+        </div>
+      </header>
+
+      <div role="main" class="inner cover">
+        <div>
+          <div class="row colors">
+            <div class="col-lg-6 col-sm-12 col-md-12 mar-img">
+              <img class="img-fluid" src="../../assets/images/farm2.jpg" />
             </div>
-        </header>
-
-        <main>
-
-            <div class="row colors">
-                <div class="col-sm mar-img"><img class="img-fluid" src="../../assets/images/farm2.jpg"></div>
-                <div class="color-div col-sm">
-                    <h1>เกษตรก้าวหน้า</h1>
-                    <p>อนาคตประเทศไทย ...เมืองอู่ข้าวอู่น้ำจะต้องดำรงคงอยู่ต่อไป วันนี้ เราอยู่นิ่งไม่ได้...เราไม่เปลี่ยนแต่โลกเปลี่ยน เราจะถูกบังคับให้เปลี่ยนด้วยเทคโนโลยีสมัยใหม่ที่จะเข้ามา เราเปลี่ยนตั้งแต่วันนี้...ทำตั้งแต่วันนี้ เพื่อเตรียมตัวรองรับสิ่งที่จะเกิดขึ้นในวันข้างหน้าให้เร็วขึ้นดีขึ้น มีความมั่นคงมากขึ้น</p>
-                </div>
-
+            <div class="color-div col-lg-6 col-sm-12 col-md-12">
+              <h1>เกษตรก้าวหน้า</h1>
+              <p>อนาคตประเทศไทย ...เมืองอู่ข้าวอู่น้ำจะต้องดำรงคงอยู่ต่อไป วันนี้ เราอยู่นิ่งไม่ได้...เราไม่เปลี่ยนแต่โลกเปลี่ยน เราจะถูกบังคับให้เปลี่ยนด้วยเทคโนโลยีสมัยใหม่ที่จะเข้ามา เราเปลี่ยนตั้งแต่วันนี้...ทำตั้งแต่วันนี้ เพื่อเตรียมตัวรองรับสิ่งที่จะเกิดขึ้นในวันข้างหน้าให้เร็วขึ้นดีขึ้น มีความมั่นคงมากขึ้น</p>
             </div>
+          </div>
+        </div>
+      </div>
 
-        </main>
-
-        <footer class="mastfoot mt-auto">
-            <nav class="mt-2">
-                <a href="http://fb.me/vetiverTH" target="_blank">Facebook</a> |
-                <a href="http://m.me/vetiverTH" target="_blank">Masseger</a>
-            </nav>
-            <div class="inner">
-                <p>©{{this.years}} เกษตรก้าวหน้า</p>
-            </div>
-        </footer>
-
+      <footer class="mastfoot mt-auto">
+        <nav class="mt-2">
+          <a href="http://fb.me/vetiverTH" target="_blank">Facebook</a> |
+          <a href="http://m.me/vetiverTH" target="_blank">Masseger</a>
+        </nav>
+        <div class="inner">
+          <p>©{{this.years}} เกษตรก้าวหน้า</p>
+        </div>
+      </footer>
     </div>
-</div>
+  </div>
 </template>
 
 <style>
@@ -46,17 +46,17 @@
 a,
 a:focus,
 a:hover {
-    color: #fff;
+  color: #fff;
 }
 
 /* Custom default button */
 .btn-secondary,
 .btn-secondary:hover,
 .btn-secondary:focus {
-    color: #333;
-    text-shadow: none;
-    background-color: rgb(45, 206, 125);
-    border: 0.05rem solid #fff;
+  color: #333;
+  text-shadow: none;
+  background-color: rgb(45, 206, 125);
+  border: 0.05rem solid #fff;
 }
 
 /*
@@ -65,159 +65,174 @@ a:hover {
 
 html,
 body {
-    font-family: "Sarabun", sans-serif;
-    height: 100%;
-    background-image: url("../../assets/images/bg_rgb.png");
-    background-size: cover;
+  font-family: "Sarabun", sans-serif;
+  height: 100%;
+  background-image: url("../../assets/images/bg_rgb.png");
+  background-size: cover;
 }
 
 body {
-    display: -ms-flexbox;
-    display: -webkit-box;
-    display: flex;
-    -ms-flex-pack: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    color: #fff;
-    text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
-    /* box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5); */
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-pack: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  color: #fff;
+  text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
+  /* box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5); */
 }
 
 .cover-container {
-    max-width: 80em;
+  max-width: 80em;
 }
 
 /*
  * Header
  */
 .masthead {
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 }
 
 .masthead-brand {
-    margin-bottom: 0;
+  margin-bottom: 0;
+  width: 200px;
+  height: auto;
 }
 
 .nav-masthead .nav-link {
-    padding: 0.25rem 0;
-    font-weight: 500;
-    color: rgba(255, 255, 255, 0.9);
-    background-color: transparent;
-    border-bottom: 0.25rem solid transparent;
+  padding: 0.25rem 0;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.9);
+  background-color: transparent;
+  border-bottom: 0.25rem solid transparent;
 }
 
 .nav-masthead .nav-link:hover,
 .nav-masthead .nav-link:focus {
-    border-bottom-color: rgba(255, 255, 255, 0.3);
+  border-bottom-color: rgba(255, 255, 255, 0.3);
 }
 
-.nav-masthead .nav-link+.nav-link {
-    margin-left: 1rem;
+.nav-masthead .nav-link + .nav-link {
+  margin-left: 1rem;
 }
 
 .nav-masthead .active {
-    color: #fff;
-    border-bottom-color: #fff;
+  color: #fff;
+  border-bottom-color: #fff;
 }
 
+@media only screen and (max-width: 600px) {
+  .masthead-brand {
+    margin-bottom: 0;
+    width: 150px;
+    height: auto;
+  }
+}
 @media (min-width: 48em) {
-    .masthead-brand {
-        float: left;
-    }
+  .masthead-brand {
+    float: left;
+    width: 150px;
+    height: auto;
+    margin: 0 0 0 15px;
+  }
 
-    .nav-masthead {
-        float: right;
-    }
+  .nav-masthead {
+    float: right;
+  }
 }
 
 /*
  * Cover
  */
 .cover {
-    padding: 0 1.5rem;
+  padding: 0 1.5rem;
+  display: flex;
+  align-items: center;
+  min-height: 80vh;
 }
 
 .cover .btn-lg {
-    padding: 0.75rem 1.25rem;
-    font-weight: 300;
-    letter-spacing: 1pt;
-    border-radius: 2rem;
+  padding: 0.75rem 1.25rem;
+  font-weight: 300;
+  letter-spacing: 1pt;
+  border-radius: 2rem;
 }
 
 /*
  * Footer
  */
 .mastfoot {
-    letter-spacing: 1.5pt;
-    /* font-size: 6pt; */
-    font-weight: 200;
-    color: rgba(233, 233, 233, 0.7);
+  letter-spacing: 1.5pt;
+  /* font-size: 6pt; */
+  font-weight: 200;
+  color: rgba(233, 233, 233, 0.7);
 }
 
 .bgg {
-    z-index: -1;
-    top: 0;
-    left: 0;
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    background-color: rgba(71, 92, 104, 0.336);
-    background-size: cover;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(71, 92, 104, 0.336);
+  background-size: cover;
 }
 
 .bg {
-    left: 0;
-    top: 0;
-    position: absolute;
-    z-index: -2;
-    height: 100%;
-    width: 100%;
-    background-image: url("../../assets/images/bg.png");
-    background-size: cover;
+  left: 0;
+  top: 0;
+  position: absolute;
+  z-index: -2;
+  height: 100%;
+  width: 100%;
+  background-image: url("../../assets/images/bg.png");
+  background-size: cover;
 }
 
 .flex-responsive {
-    display: flex;
-    text-align: center;
+  display: flex;
+  text-align: center;
 }
 
 .color-div {
-    background-color: #777;
-    text-align: left;
-    width: 100%;
-    padding-left: 2%;
-    padding-top: 2%;
+  background-color: #777;
+  text-align: left;
+  width: 100%;
+  padding-left: 2%;
+  padding-top: 2%;
 }
 
 .colors {
-    background-color: #777;
-    padding: 0;
-    margin: 3%;
-    margin-bottom: 0;
+  background-color: #777;
+  padding: 0;
+  margin: 3%;
+  margin-bottom: 0;
 }
 
 .mar-img {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
 </style>
 
 <script>
 export default {
-    name: "HomePage",
-    data() {
-        return {
-            years: null
-        };
-    },
-    methods: {
-        getYears() {
-            var date = new Date();
-            this.years = date.getFullYear();
-        }
-    },
-    mounted() {
-        this.getYears();
+  name: "HomePage",
+  data() {
+    return {
+      years: null
+    };
+  },
+  methods: {
+    getYears() {
+      var date = new Date();
+      this.years = date.getFullYear();
     }
+  },
+  mounted() {
+    this.getYears();
+  }
 };
 </script>
--- src/views/pages/Homepage.vue
+++ src/views/pages/Homepage.vue
@@ -1,38 +1,45 @@
 <template>
-<div class="text-center">
+  <div class="text-center">
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-        <header class="masthead mb-auto">
-            <div class="inner">
-                <img src="../../assets/images/logo_vetiver.png" class="masthead-brand" width="15%" />
-                <nav class="nav nav-masthead justify-content-center">
-                    <a class="nav-link" href="http://localhost:8080/homepage">หน้าหลัก</a>
-                    <a class="nav-link" href="http://localhost:8080/aboutme">เกี่ยวกับเรา</a>
-                    <a class="nav-link" href="http://localhost:8080/contact">คุยกับเรา</a>
-                    <a class="nav-link" href="#">ร่วมงานกับเรา</a>
-                </nav>
-            </div>
-        </header>
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <img src="../../assets/images/logo_vetiver.png" class="masthead-brand" width="15%" />
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="http://localhost:8080/homepage">หน้าหลัก</a>
+            <a class="nav-link" href="http://localhost:8080/aboutme">เกี่ยวกับเรา</a>
+            <a class="nav-link" href="http://localhost:8080/contact">คุยกับเรา</a>
+            <a class="nav-link" href="#">ร่วมงานกับเรา</a>
+          </nav>
+        </div>
+      </header>
 
-        <main role="main" class="inner cover">
-            <h1 class="cover-heading" style="font-size: 48pt">เกษตรก้าวหน้า</h1>
-            <h6 class="mt-5 mb-0">การจะพัฒนาทุกสิ่งทุกอย่างให้เจริญนั้น จะต้องสร้างและเสริมขึ้นจากพื้นฐานเดิมที่มีอยู่ก่อนทั้งสิ้น</h6>
-            <br />
-            <h4 class="mt-0 mb-0">ถ้าพื้นฐานไม่ดีหรือคลอนแคลนบกพร่องแล้ว การที่จะเพิ่มเติมเสริมต่อให้เจริญขึ้นไปอีกนั้นเป็นเรื่องยาก</h4>
-            <br />
-            <h6 class="mt-0">จึงควรจะเข้าใจให้แจ้งชัดว่า นอกจากจะมุ่งสร้างความเจริญแล้ว ยังต้องพยายามรักษาพื้นฐานให้มั่นคง ไม่บกพร่อง พร้อม ๆ กันได้</h6>
-            <p class="lead mt-5">
-                <a href="http://localhost:8080/login" class="btn btn-lg btn-primary">เข้าใช้งาน</a>
-            </p>
-        </main>
+      <div role="main" class="inner cover">
+        <div>
+          <h1 class="cover-heading" style="font-size: 48pt">เกษตรก้าวหน้า</h1>
+          <h6
+            class="mt-5 mb-0"
+          >การจะพัฒนาทุกสิ่งทุกอย่างให้เจริญนั้น จะต้องสร้างและเสริมขึ้นจากพื้นฐานเดิมที่มีอยู่ก่อนทั้งสิ้น</h6>
+          <br />
+          <h4
+            class="mt-0 mb-0"
+          >ถ้าพื้นฐานไม่ดีหรือคลอนแคลนบกพร่องแล้ว การที่จะเพิ่มเติมเสริมต่อให้เจริญขึ้นไปอีกนั้นเป็นเรื่องยาก</h4>
+          <br />
+          <h6
+            class="mt-0"
+          >จึงควรจะเข้าใจให้แจ้งชัดว่า นอกจากจะมุ่งสร้างความเจริญแล้ว ยังต้องพยายามรักษาพื้นฐานให้มั่นคง ไม่บกพร่อง พร้อม ๆ กันได้</h6>
+          <p class="lead mt-5">
+            <a href="http://localhost:8080/login" class="btn btn-lg btn-primary">เข้าใช้งาน</a>
+          </p>
+        </div>
+      </div>
 
-        <footer class="mastfoot mt-auto">
-            <div class="inner">
-                <p>©{{this.years}} เกษตรก้าวหน้า</p>
-            </div>
-        </footer>
-        
+      <footer class="mastfoot mt-auto mt-5">
+        <div class="inner">
+          <p>©{{this.years}} เกษตรก้าวหน้า</p>
+        </div>
+      </footer>
     </div>
-</div>
+  </div>
 </template>
 
 <style>
@@ -41,17 +48,17 @@
 a,
 a:focus,
 a:hover {
-    color: #fff;
+  color: #fff;
 }
 
 /* Custom default button */
 .btn-secondary,
 .btn-secondary:hover,
 .btn-secondary:focus {
-    color: #333;
-    text-shadow: none;
-    background-color: rgb(45, 206, 125);
-    border: 0.05rem solid #fff;
+  color: #333;
+  text-shadow: none;
+  background-color: rgb(45, 206, 125);
+  border: 0.05rem solid #fff;
 }
 
 /*
@@ -60,70 +67,98 @@ a:hover {
 
 html,
 body {
-    font-family: "Sarabun", sans-serif;
-    height: 100%;
-    width: 100%;
-    background-image: url("../../assets/images/bg_rgb.png");
-    background-size: cover;
+  font-family: "Sarabun", sans-serif;
+  background-image: url("../../assets/images/bg_rgb.png");
+  background-size: cover;
 }
 
 body {
-    display: -ms-flexbox;
-    display: -webkit-box;
-    display: flex;
-    -ms-flex-pack: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    color: #fff;
-    text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
-    /* box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5); */
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-pack: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  color: #fff;
+  text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
+  /* box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5); */
 }
 
 .cover-container {
-    max-width: 80em;
+  max-width: 80em;
 }
 
 /*
  * Header
  */
 .masthead {
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 }
 
 .masthead-brand {
-    margin-bottom: 0;
+  margin-bottom: 0;
+  width: 200px;
+  height: auto;
 }
 
 .nav-masthead .nav-link {
-    padding: 0.25rem 0;
-    font-weight: 500;
-    color: rgba(255, 255, 255, 0.9);
-    background-color: transparent;
-    border-bottom: 0.25rem solid transparent;
+  padding: 0.25rem 0;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.9);
+  background-color: transparent;
+  border-bottom: 0.25rem solid transparent;
 }
 
 .nav-masthead .nav-link:hover,
 .nav-masthead .nav-link:focus {
-    border-bottom-color: rgba(255, 255, 255, 0.3);
+  border-bottom-color: rgba(255, 255, 255, 0.3);
 }
 
-.nav-masthead .nav-link+.nav-link {
-    margin-left: 1rem;
+.nav-masthead .nav-link + .nav-link {
+  margin-left: 1rem;
 }
 
 .nav-masthead .active {
-    color: #fff;
-    border-bottom-color: #fff;
+  color: #fff;
+  border-bottom-color: #fff;
+}
+
+@media only screen and (max-width: 600px) {
+  .masthead-brand {
+    margin-bottom: 0;
+    width: 150px;
+    height: auto;
+  }
+    body {
+    font-family: "Sarabun", sans-serif;
+    background-image: url("../../assets/images/bg_rgb.png");
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+  
 }
 
 @media (min-width: 48em) {
-    .masthead-brand {
-        float: left;
-    }
+  .masthead-brand {
+    float: left;
+    width: 150px;
+    height: auto;
+    margin: 0 0 0 15px;
+  }
+    body {
+    font-family: "Sarabun", sans-serif;
+    background-image: url("../../assets/images/bg_rgb.png");
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
 
-    .nav-masthead {
-        float: right;
-    }
+  .nav-masthead {
+    float: right;
+  }
 }
 
 /*
@@ -131,44 +166,46 @@ body {
  */
 
 .cover {
-    padding: 0 1.5rem;
+  padding: 0 1.5rem;
+  display: flex;
+  align-items: center;
+  min-height: 80vh;
 }
 
 .cover .btn-lg {
-    padding: 0.75rem 1.25rem;
-    font-weight: 300;
-    letter-spacing: 1pt;
-    border-radius: 2rem;
+  padding: 0.75rem 1.25rem;
+  font-weight: 300;
+  letter-spacing: 1pt;
+  border-radius: 2rem;
 }
 
 /*
  * Footer
  */
 .mastfoot {
-    letter-spacing: 1.5pt;
-    /* font-size: 6pt; */
-    font-weight: 200;
-    color: rgba(233, 233, 233, 0.7);
+  letter-spacing: 1.5pt;
+  /* font-size: 6pt; */
+  font-weight: 200;
+  color: rgba(233, 233, 233, 0.7);
 }
-
 </style>
 
 <script>
 export default {
-    name: "HomePage",
-    data() {
-        return {
-            years: null
-        };
-    },
-    methods: {
-        getYears() {
-            var date = new Date();
-            this.years = date.getFullYear();
-        }
-    },
-    mounted() {
-        this.getYears();
+  name: "HomePage",
+  data() {
+    return {
+      years: null
+    };
+  },
+  methods: {
+    getYears() {
+      var date = new Date();
+      this.years = date.getFullYear();
     }
+  },
+  mounted() {
+    this.getYears();
+  }
 };
 </script>
